316016f6087382027eb8e21f9eede38c1cb7ec71
binayak-dangi <binayakdangi@gmail.com>
2025-06-02 12:18:42 +0545

    server setup install dotenv morgan router

---
+++ server.js
@@ -0,0 +1,22 @@
+const express=require('express');
+const dotenv=require('dotenv').config();
+const morgan=require('morgan');
+
+
+
+const app=express();
+
+
+// Middleware
+app.use(morgan('dev'));
+
+//Middleware Routes
+app.get('/',(req,res)=>{
+    res.send('API is running...');
+})
+
+const PORT=process.env.PORT||5000;
+
+app.listen(PORT,()=>{
+    console.log(`Server is running on port ${PORT}`);
+})
